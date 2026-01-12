6de53b54e93048cb892594240040774281f77322
seyha yoeun <yoeunseyha11@gmail.com>
2026-01-13 00:11:26 +0700

    Create File Structure

---
+++ food-delivery-website/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="">
+      <h2 className="text-red-700">Hello Wold</h2>
+    </div>
+  );
+};
+
+export default App;
